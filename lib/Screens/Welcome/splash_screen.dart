8c627afb4b8c1fd0a94f9765e99936a37813f2d6
Bhaskar Gupta <bhaskargupta1997@gmail.com>
2021-09-29 13:07:08 +0530

    Refactored

--- lib/Screens/Welcome/splash_screen.dart
+++ lib/Screens/Welcome/splash_screen.dart
@@ -1,39 +1,165 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:project_open_jung/Screens/Welcome/intro_slider.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
+import 'package:project_open_jung/Screens/splash_screen.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new SplashScreen2(),
-  ));
-}
-
-class SplashScreen2 extends StatefulWidget {
-  @override
-  _SplashScreen2State createState() => new _SplashScreen2State();
-}
-
-class _SplashScreen2State extends State<SplashScreen2> {
+import 'intro_slider.dart';
+class Root extends SplashScreen
+{
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-      seconds: 10,
-      navigateAfterSeconds: new AfterSplash(),
-      title: new Text(
-        'Welcome In SplashScreen',
-        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-      ),
-      image: new Image.asset('images/icon.png',scale: 10,),
-      backgroundColor: Colors.white,
-      loaderColor: Colors.red,
+    return GetX(
+      initState: (_) {
+        getStringValuesSF();
+        getDoubleValuesSFIE();
+        getDoubleValuesSFSN();
+        getDoubleValuesSFFT();
+        getDoubleValuesSFJP();
+        getDoubleValuesSFIER();
+        getDoubleValuesSFSNR();
+        getDoubleValuesSFFTR();
+        getDoubleValuesSFJPR();
+
+      },
+      builder: (_) {
+        if (result == null) {
+          return  SplashScreen();
+        } else {
+          return IntroSlider();
+        }
+      },
     );
   }
 }
 
-class AfterSplash extends StatelessWidget {
-  int i = 0;
+
+
+
+typedef GetXControllerBuilder<T extends DisposableInterface> = Widget Function(
+    T controller);
+
+class GetX<T extends DisposableInterface> extends StatefulWidget {
+  final GetXControllerBuilder<T> builder;
+  final bool global;
+
+  // final Stream Function(T) stream;
+  // final StreamController Function(T) streamController;
+  final bool autoRemove;
+  final bool assignId;
+  final void Function(State state) initState, dispose, didChangeDependencies;
+  final void Function(GetX oldWidget, State state) didUpdateWidget;
+  final T init;
+  final String tag;
+
+  const GetX({
+    this.tag,
+    this.builder,
+    this.global = true,
+    this.autoRemove = true,
+    this.initState,
+    this.assignId = false,
+    //  this.stream,
+    this.dispose,
+    this.didChangeDependencies,
+    this.didUpdateWidget,
+    this.init,
+    // this.streamController
+  });
+
   @override
-  Widget build(BuildContext context) {
-    return  i==1?IntroSlider():SplashScreen();
-  }
+  GetXState<T> createState() => GetXState<T>();
 }
+
+class GetXState<T extends DisposableInterface> extends State<GetX<T>> {
+  GetXState() {
+    _observer = RxNotifier();
+  }
+  RxInterface _observer;
+  T controller;
+  bool isCreator = false;
+  StreamSubscription subs;
+
+  @override
+  void initState() {
+    // var isPrepared = GetInstance().isPrepared<T>(tag: widget.tag);
+    var isRegistered = GetInstance().isRegistered<T>(tag: widget.tag);
+
+    if (widget.global) {
+      if (isRegistered) {
+        if (GetInstance().isPrepared<T>(tag: widget.tag)) {
+          isCreator = true;
+        } else {
+          isCreator = false;
+        }
+        controller = GetInstance().find<T>(tag: widget.tag);
+      } else {
+        controller = widget.init;
+        isCreator = true;
+        GetInstance().put<T>(controller, tag: widget.tag);
+      }
+    } else {
+      controller = widget.init;
+      isCreator = true;
+      controller?.onStart();
+    }
+    widget.initState?.call(this);
+    if (widget.global && Get.smartManagement == SmartManagement.onlyBuilder) {
+      controller?.onStart();
+    }
+    subs = _observer.listen((data) => setState(() {}));
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (widget.didChangeDependencies != null) {
+      widget.didChangeDependencies(this);
+    }
+  }
+
+  @override
+  void didUpdateWidget(GetX oldWidget) {
+    super.didUpdateWidget(oldWidget as GetX<T>);
+    if (widget.didUpdateWidget != null) widget.didUpdateWidget(oldWidget, this);
+  }
+
+  @override
+  void dispose() {
+    if (widget.dispose != null) widget.dispose(this);
+    if (isCreator || widget.assignId) {
+      if (widget.autoRemove && GetInstance().isRegistered<T>(tag: widget.tag)) {
+        GetInstance().delete<T>(tag: widget.tag);
+      }
+    }
+    subs.cancel();
+    _observer.close();
+    controller = null;
+    isCreator = null;
+    super.dispose();
+  }
+
+  Widget get notifyChildren {
+    final observer = RxInterface.proxy;
+    RxInterface.proxy = _observer;
+    final result = widget.builder(controller);
+    if (!_observer.canUpdate) {
+      throw """
+      [Get] the improper use of a GetX has been detected. 
+      You should only use GetX or Obx for the specific widget that will be updated.
+      If you are seeing this error, you probably did not insert any observable variables into GetX/Obx 
+      or insert them outside the scope that GetX considers suitable for an update 
+      (example: GetX => HeavyWidget => variableObservable).
+      If you need to update a parent widget and a child widget, wrap each one in an Obx/GetX.
+      """;
+    }
+    RxInterface.proxy = observer;
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) => notifyChildren;
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,57 @@
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/screenutil_init.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_open_jung/Screens/Welcome/splash_screen.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:project_open_jung/Screens/splash_screen.dart';
 
 
-
-
-void main() {
+FirebaseAnalytics analytics = FirebaseAnalytics();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
+    return ScreenUtilInit(
+        designSize: Size(360,690),
+        allowFontScaling: false,
+        builder: ()=> MaterialApp(
+            navigatorObservers: [
+              FirebaseAnalyticsObserver(analytics: analytics),
+            ],
+            theme: ThemeData.dark().copyWith(
+                backgroundColor: Colors.black,
+                textTheme: GoogleFonts.poppinsTextTheme(
+                  Theme.of(context).textTheme,
+                )
+            ),
+            debugShowCheckedModeBanner: false,
+            home: FutureBuilder(
+              future: _fbApp,
+              builder: (context, snapshot){
+                if(snapshot.hasError){
+                  print(snapshot.error.toString());
+                  return Text('Something went wrong');
+                } else if(snapshot.hasData){
+                  return StreamBuilder(
+                      builder: (context, data) => SplashScreen());
+                } else{
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )
+
+
+        ));
   }
 }
+
